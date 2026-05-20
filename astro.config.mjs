b932b426f2e02136a5ebd2900648328f0aec1d48
gabygarcia-icbvina <gabygarcia@insucoabg.cl>
2026-05-20 14:38:59 -0400

    Fixing Beta-2.0 Deploy Error

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
